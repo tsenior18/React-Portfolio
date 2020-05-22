f813a7650322600f46cdf0b537d6b1955ff71ed4
Nanhe Kumar <tomsenior18@gmail.com>
2020-05-22 15:23:01 +0100

    Login Section styling done

--- src/login.js
+++ src/login.js
@@ -8,13 +8,13 @@ class Login extends Component {
             <div className="Login">
             <div className='title'>
                 <h1>Log In Page</h1>
-            </div>
             <div className="inputs">
                 <label>{this.props.user}</label>
-                <input type="text" placeholder={this.props.user}></input>
+                <input className="input" type="text" placeholder={this.props.user}></input>
                 <label>{this.props.pass}</label>
-                <input type="password" placeholder={this.props.pass}></input>
+                <input className="input" type="password" placeholder={this.props.pass}></input>
                 <a href="#home">{this.props.buttonName}</a>
+            </div>
             </div>
             </div>
         );
